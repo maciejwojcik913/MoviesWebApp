c21c51721bf2a722d4b5a7fe560753de5b8979b0
Maciej Wójcik <maciejwojcik913@gmail.com>
2023-02-22 21:20:37 +0100

    Table name correction.

--- src/main/java/io/github/maciejwojcik913/MoviesWebApp/Domain/CountryEntity.java
+++ src/main/java/io/github/maciejwojcik913/MoviesWebApp/Domain/CountryEntity.java
@@ -5,7 +5,7 @@ import io.github.maciejwojcik913.MoviesWebApp.Domain.Abstract.AbstractEntity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "genre")
+@Table(name = "country")
 public class CountryEntity extends AbstractEntity {
 
     @Id @Column(nullable = false, length = 30)
