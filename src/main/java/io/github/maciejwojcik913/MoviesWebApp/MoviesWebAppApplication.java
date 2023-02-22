ebca9df7654bc48ea61d55c8fe1f9ceb6e404bc5
Maciej Wójcik <maciejwojcik913@gmail.com>
2023-02-22 17:30:08 +0100

    Initial commit - preparing application.

---
+++ src/main/java/io/github/maciejwojcik913/MoviesWebApp/MoviesWebAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.maciejwojcik913.MoviesWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/maciejwojcik913/MoviesWebApp/MoviesWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maciejwojcik913.MoviesWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
