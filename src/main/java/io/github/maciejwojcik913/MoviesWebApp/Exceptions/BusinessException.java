a259cfb165388ddebdd0c6e4b42b127925e6ee3f
Maciej Wójcik <maciejwojcik913@gmail.com>
2023-02-23 02:23:51 +0100

    Creating custom exception classes.

---
+++ src/main/java/io/github/maciejwojcik913/MoviesWebApp/Exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package io.github.maciejwojcik913.MoviesWebApp.Exceptions;
+
+class BusinessException extends RuntimeException{
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/github/maciejwojcik913/MoviesWebApp/Exceptions/IncorrectTransferObjectException.java
@@ -0,0 +1,8 @@
+package io.github.maciejwojcik913.MoviesWebApp.Exceptions;
+
+public class IncorrectTransferObjectException extends BusinessException{
+
+    public IncorrectTransferObjectException(String message) {
+        super(message);
+    }
+}
