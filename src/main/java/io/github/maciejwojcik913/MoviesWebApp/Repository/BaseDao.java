cd5088324a7f7673d3f64eaf23677f0efa589472
Maciej Wójcik <maciejwojcik913@gmail.com>
2023-03-10 15:19:11 +0100

    Update of BaseDao

--- src/main/java/io/github/maciejwojcik913/MoviesWebApp/Repository/BaseDao.java
+++ src/main/java/io/github/maciejwojcik913/MoviesWebApp/Repository/BaseDao.java
@@ -4,7 +4,14 @@ import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface BaseDao<T, ID extends Serializable> extends PagingAndSortingRepository<T, ID> {
+
+    @Override
+    List<T> findAll();
+
+    Optional<T> findById(ID id);
 }
