8d860dececb14f19d33f6fcca0ed8f08ab21e71d
Maciej Wójcik <maciejwojcik913@gmail.com>
2023-03-10 15:16:49 +0100

    Editing HomeController and testing.

--- src/main/java/io/github/maciejwojcik913/MoviesWebApp/Controller/HomeController.java
+++ src/main/java/io/github/maciejwojcik913/MoviesWebApp/Controller/HomeController.java
@@ -1,20 +1,25 @@
 package io.github.maciejwojcik913.MoviesWebApp.Controller;
 
+import io.github.maciejwojcik913.MoviesWebApp.Services.HomeService;
 import io.github.maciejwojcik913.MoviesWebApp.ViewsNames;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = {"", "/", "/home"})
 public class HomeController {
 
+    @Autowired
+    private HomeService homeService;
+
     @GetMapping
-    public ModelAndView getWelcome() {
-        var mav = new ModelAndView();
-        mav.setViewName(ViewsNames.HOME_PAGE);
-        mav.addObject("greeting", "Hello Guest");
+    public ModelAndView getWelcome(@RequestParam(name = "name", required = false) String name) {
+        var mav = new ModelAndView(ViewsNames.HOME_PAGE);
+        mav.addObject("greeting", homeService.prepareGreeting(name));
         return mav;
     }
 }
